04ecbb420aa7ad6558805399c6dacead3466360b
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-06-19 00:26:12 +0530

    Dashboard components

--- client/src/pages/Dashboard/Cards.jsx
+++ client/src/pages/Dashboard/Cards.jsx
@@ -4,8 +4,8 @@ const Cards = () => {
         return (
             <div className="row" id="rowofcards">
                 <div className="col-4">
-                    <div className="card">
-                        <div className="" id="user-card">
+                    <div className="" id="user-card">
+                        <div className="card-body" id="user-card">
                             <h5 className="card-title">Total Users</h5>
                             <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                             <a href="/users" className="btn btn-primary">View Users</a>
@@ -13,20 +13,20 @@ const Cards = () => {
                     </div>
                 </div>
                 <div className="col-4">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Total Products</h5>
+                    <div className="" id="user-card">
+                        <div className="card-body" id="user-card">
+                            <h5 className="card-title">Total Users</h5>
                             <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="/products" className="btn btn-primary">View Products</a>
+                            <a href="/users" className="btn btn-primary">View Users</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-4">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Total Orders</h5>
+                    <div className="" id="user-card">
+                        <div className="card-body" id="user-card">
+                            <h5 className="card-title">Total Users</h5>
                             <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="/orders" className="btn btn-primary">View Orders</a>
+                            <a href="/users" className="btn btn-primary">View Users</a>
                         </div>
                     </div>
                 </div>
